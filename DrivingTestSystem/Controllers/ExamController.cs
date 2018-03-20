ab59045db880cf41b3dd9723de3bb67d3d76c21a
LiuXinyuan <liuxinyuan_victory@163.com>
2018-03-20 11:16:06 +0800

    新题库

--- DrivingTestSystem/Controllers/ExamController.cs
+++ DrivingTestSystem/Controllers/ExamController.cs
@@ -13,6 +13,10 @@ namespace DrivingTestSystem.Controllers
         //
         // GET: /Exam/
         private SubjectContext sc = new SubjectContext();
+        private Subject2Entities s2 = new Subject2Entities();
+        private Subject2_PicEntities1 pic = new Subject2_PicEntities1();
+        private Subject2_SoundEntities sound = new Subject2_SoundEntities();
+
         private Subject[] getRandomList(Subject[] oldList)
         {
             if (oldList.Length == 1)
@@ -63,13 +67,78 @@ namespace DrivingTestSystem.Controllers
         [HttpGet]
         public ActionResult OnePuzzle()
         {
+            EXAM_TK[] tk = s2.EXAM_TK.ToArray();
+            List<Subject> list = new List<Subject>();
+            for (int i = 0; i < tk.Length; i++)
+            {
+                Subject s = new Subject();
+                s.subject_content = tk[i].STNRH;
+                s.subject_content_tibetan = tk[i].STNRZ;
+                if (tk[i].STTX.Equals("2")) //选择题
+                {
+                    s.subject_A = tk[i].XZDAAH;
+                    s.subject_A_tibetan = tk[i].XZDAAZ;
+
+                    s.subject_B = tk[i].XZDABH;
+                    s.subject_B_tibetan = tk[i].XZDABZ;
+
+                    s.subject_C = tk[i].XZDACH;
+                    s.subject_C_tibetan = tk[i].XZDACZ;
+
+                    s.subject_D = tk[i].XZDADH;
+                    s.subject_D_tibetan = tk[i].XZDADZ;
+
+                    s.subject_type = "C";
+                }
+                else {
+                    s.subject_A = "正确";
+                    s.subject_A_tibetan = "འགྲིག་པ།";
+                    s.subject_B = "错误";
+                    s.subject_B_tibetan = "ནོར་བ།";
+                    s.subject_type = "J";
+                }
+                switch (tk[i].STDA)
+                {
+                    case "1":
+                        s.subject_answer = "A";
+                        break;
+                    case "2":
+                        s.subject_answer = "B";
+                        break;
+                    case "3":
+                        s.subject_answer = "C";
+                        break;
+                    case "4":
+                        s.subject_answer = "D";
+                        break;
+                    case "5":
+                        s.subject_answer = "A";
+                        break;
+                    case "6":
+                        s.subject_answer = "B";
+                        break;
+                }
+                s.subject_id = int.Parse(tk[i].STXH);
+                list.Add(s);
+            }
             string lan = (string)Session["language"];
-            Subject[] sOld = sc.SubjectList.ToArray();
-            Subject[] sArray = getRandomList(sOld);
+            //Subject[] sOld = sc.SubjectList.ToArray();
+            Subject[] sArray = getRandomList(list.ToArray());
             List<Subject> s_a = new List<Subject>();
             int len = sArray.Length > 100 ? 100 : sArray.Length;
             ViewBag.len=len;
             for (int i = 0; i < len; i++) {
+                string xh = sArray[i].subject_id.ToString();
+                var pics = pic.EXAM_TK_TP.Where(p => p.STXH == xh).ToList();
+                var sounds = sound.EXAM_TK_YY_ADY.Where(tk_s => tk_s.STXH == xh).ToList();
+                if (pics.Count() > 0)
+                {
+                    sArray[i].subject_img = pics[0].STXH;
+                }
+                if (sounds.Count() > 0)
+                {
+                    sArray[i].subject_sound_content_tibetan = sounds[0].STXH;
+                }
                 s_a.Add(sArray[i]);
             }
             ViewBag.nowIndex = 0;
@@ -80,25 +149,120 @@ namespace DrivingTestSystem.Controllers
         [HttpGet]
         public ActionResult FourPuzzle() //科目四每题两分
         {
+            EXAM_TK[] tk = s2.EXAM_TK.ToArray();
+            List<Subject> list = new List<Subject>();
+            for (int i = 0; i < tk.Length; i++)
+            {
+                Subject s = new Subject();
+                s.subject_content = tk[i].STNRH;
+                s.subject_content_tibetan = tk[i].STNRZ;
+                if (tk[i].STTX.Equals("2")) //选择题
+                {
+                    s.subject_A = tk[i].XZDAAH;
+                    s.subject_A_tibetan = tk[i].XZDAAZ;
+
+                    s.subject_B = tk[i].XZDABH;
+                    s.subject_B_tibetan = tk[i].XZDABZ;
+
+                    s.subject_C = tk[i].XZDACH;
+                    s.subject_C_tibetan = tk[i].XZDACZ;
+
+                    s.subject_D = tk[i].XZDADH;
+                    s.subject_D_tibetan = tk[i].XZDADZ;
+
+                    s.subject_type = "C";
+                }
+                else
+                {
+                    s.subject_A = "正确";
+                    s.subject_A_tibetan = "འགྲིག་པ།";
+                    s.subject_B = "错误";
+                    s.subject_B_tibetan = "ནོར་བ།";
+                    s.subject_type = "J";
+                }
+                switch (tk[i].STDA)
+                {
+                    case "1":
+                        s.subject_answer = "A";
+                        break;
+                    case "2":
+                        s.subject_answer = "B";
+                        break;
+                    case "3":
+                        s.subject_answer = "C";
+                        break;
+                    case "4":
+                        s.subject_answer = "D";
+                        break;
+                    case "5":
+                        s.subject_answer = "A";
+                        break;
+                    case "6":
+                        s.subject_answer = "B";
+                        break;
+                }
+                s.subject_id = int.Parse(tk[i].STXH);
+                list.Add(s);
+            }
             string lan = (string)Session["language"];
-            Subject[] sOld = sc.SubjectList.ToArray();
-            Subject[] sArray = getRandomList(sOld);
-            ViewBag.len = sArray.Length > 50 ? 50 :sArray.Length;
+            //Subject[] sOld = sc.SubjectList.ToArray();
+            Subject[] sArray = getRandomList(list.ToArray());
+            List<Subject> s_a = new List<Subject>();
+            int len = sArray.Length > 50 ? 50 : sArray.Length;
+            ViewBag.len = len;
+            for (int i = 0; i < len; i++)
+            {
+                string xh = sArray[i].subject_id.ToString();
+                var pics = pic.EXAM_TK_TP.Where(p => p.STXH == xh).ToList();
+                var sounds = sound.EXAM_TK_YY_ADY.Where(tk_s => tk_s.STXH == xh).ToList();
+                if (pics.Count() > 0)
+                {
+                    sArray[i].subject_img = pics[0].STXH;
+                }
+                if (sounds.Count() > 0)
+                {
+                    sArray[i].subject_sound_content_tibetan = sounds[0].STXH;
+                }
+                s_a.Add(sArray[i]);
+            }
             ViewBag.nowIndex = 0;
             ViewBag.language = lan;
-            return View(sArray);
+            return View(s_a.ToArray());
         }
         [HttpPost]
         public JsonResult CheckAnswer(string user_answer, int subject_id)
         {
-            Subject exist = sc.SubjectList.Where(s => s.subject_id == subject_id).FirstOrDefault();
-            if (exist.subject_answer == user_answer)
+            string id = subject_id.ToString();
+            var exist = s2.EXAM_TK.Where(tk => tk.STXH == id).FirstOrDefault();
+            string subject_answer = "A";
+            switch (exist.STDA)
+            {
+                case "1":
+                    subject_answer = "A";
+                    break;
+                case "2":
+                    subject_answer = "B";
+                    break;
+                case "3":
+                    subject_answer = "C";
+                    break;
+                case "4":
+                    subject_answer = "D";
+                    break;
+                case "5":
+                    subject_answer = "A";
+                    break;
+                case "6":
+                    subject_answer = "B";
+                    break;
+            }
+            if (subject_answer == user_answer)
             {
                 return Json("ok");
             }
             else
             {
-                return Json(exist.subject_answer);
+                return Json(subject_answer);
             }
         }
     }
---
+++ DrivingTestSystem/Controllers/Subject2InfoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DrivingTestSystem.Utils;
+using DrivingTestSystem.Models.Subject;
+
+namespace DrivingTestSystem.Utils
+{
+    public class Subject2InfoController : Controller
+    {
+        //
+        // GET: /Subject2Info/
+        private Subject2_PicEntities1 pics = new Subject2_PicEntities1();
+        private Subject2_SoundEntities sounds = new Subject2_SoundEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult getPic(string xh="10029")
+        {
+            if (xh != null || xh != "")
+            {
+                var pic = pics.EXAM_TK_TP.Where(p => p.STXH == xh).FirstOrDefault();
+                byte[] bytes = Convert.FromBase64String(pic.TPNR);
+                return File(bytes, "image/gif");
+            }
+            else
+            {
+                return Content("");
+            }
+        }
+        [HttpGet]
+        public ActionResult getSound(string xh)
+        {
+            if (xh != null || xh != "")
+            {
+                var sound = sounds.EXAM_TK_YY_ADY.Where(tk_s => tk_s.STXH == xh).FirstOrDefault();
+                byte[] bytes = Convert.FromBase64String(sound.YYNR);
+                return File(bytes, "audio/mp3");
+            }
+            else
+            {
+                return Content("");
+            }
+        }
+
+    }
+}
